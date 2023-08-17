eb0c6e6bf5158e091e325add416966105a6ab2c0
yesseruser <yesseruseryt@gmail.com>
2023-08-17 12:28:49 +0200

    Fixed bug where jar won't run

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,17 @@ plugins {
 }
 
 group = "com.yesser-studios"
-version = "1.0-SNAPSHOT"
+version = "1.1"
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "com.yesserstudios.javatutorialbutinkotlin.KotlinClassKt"
+    }
+    configurations["compileClasspath"].forEach { file: File ->
+        from(zipTree(file.absoluteFile))
+    }
+    duplicatesStrategy = DuplicatesStrategy.INCLUDE
+}
 
 repositories {
     mavenCentral()
